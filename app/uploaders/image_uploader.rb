d93859808afda67932f6461c2735fe7a3435bc0b
jcgertig <jcgertig@gmail.com>
2015-04-09 00:52:15 -0400

    Fix Image Uploads to use fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
-  # storage :fog
-  storage :file
+  storage :fog
+  
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
