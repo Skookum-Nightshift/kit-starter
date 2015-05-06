88144cc1a1ea48696e5107da3d1c4d310aa4b666
James Thullbery <ifndef@gmail.com>
2015-05-05 20:42:23 -0400

    updated routes and pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'homepage#index'
-  get 'choose' => 'homepage#choose'
-  get 'drop_off' => 'homepage#drop_off'
-  get 'donate' => 'homepage#donate'
-  get 'share' => 'homepage#share'
-  get 'team'  => 'homepage#team'
-  get 'others' => 'homepage#others'
-  get 'hope' => 'homepage#hope'
+  get  'choose' => 'homepage#choose'
+  get  'drop_off' => 'homepage#drop_off'
+  get  'donate' => 'homepage#donate'
+  get  'share' => 'homepage#share'
+  post 'share' => 'homepage#share'
+  get  'team'  => 'homepage#team'
+  get  'others' => 'homepage#others'
+  get  'hope' => 'homepage#hope'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
